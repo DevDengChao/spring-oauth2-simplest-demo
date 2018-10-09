9c2349bb2c9f9101c576f2ec7e37298caf714a84
XieEDeHeiShou <2325690622@qq.com>
2018-10-10 00:07:34 +0800

    add WebSecurityConfig: init user

---
+++ src/main/java/demo/oauth2/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package demo.oauth2;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity(debug = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public WebSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        // nothing need to do here, token endpoint is public to anonymous user by default.
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // super.configure(auth);
+
+        auth.eraseCredentials(true)
+                .inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("myUsername")
+                .password(passwordEncoder.encode("myPassword"))
+                .roles("USER");
+    }
+}
