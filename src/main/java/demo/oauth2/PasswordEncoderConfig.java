03533fd79314d4c838a5fd520a251c764756601a
XieEDeHeiShou <2325690622@qq.com>
2018-10-10 00:04:20 +0800

    add PasswordEncoderConfig

---
+++ src/main/java/demo/oauth2/PasswordEncoderConfig.java
@@ -0,0 +1,18 @@
+package demo.oauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author <a href="mailto:qq2325690622@gmail.com">邓超</a> on 2018/10/8
+ */
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
