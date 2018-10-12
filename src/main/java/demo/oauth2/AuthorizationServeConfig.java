93ce8d58bbcd4e0c3dcdaf67cc57ae429fa23896
XieEDeHeiShou@xgtl <2325690622@qq.com>
2018-10-12 14:27:59 +0800

    override methods (no-op)

--- src/main/java/demo/oauth2/AuthorizationServeConfig.java
+++ src/main/java/demo/oauth2/AuthorizationServeConfig.java
@@ -1,8 +1,11 @@
 package demo.oauth2;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -14,4 +17,18 @@ public class AuthorizationServeConfig extends AuthorizationServerConfigurerAdapt
     //     "error_description": "Full authentication is required to access this resource"
     // }
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        super.configure(clients);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+    }
 }
